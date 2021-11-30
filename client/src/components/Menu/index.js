3101f711e1a2788dbd0109ae9c85f492e067fb32
JacksonWhite4725 <jawh4725@colorado.edu>
2021-11-30 09:41:56 -0700

    Added more menu function for auth verification

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -4,7 +4,26 @@ import { Link } from 'react-router-dom';
 import './menu.css';
 
 function Menu({ menuOpen, setMenuOpen }) {
-    return(
+    function showAccount() {
+        if (Auth.loggedIn()) {
+            return (
+                <li onClick={()=>setMenuOpen(false)}>
+                    <Link to='/account'>
+                        My Account
+                    </Link>
+                </li>
+            );
+        } else {
+            return (
+                <li onClick={()=>setMenuOpen(false)}>
+                    <Link to='/login'>
+                        Login
+                    </Link>
+                </li>
+            );
+        }
+    }
+    return (
         <div className={'menu ' + (menuOpen && 'active')}>
             <ul>
                 <li onClick={()=>setMenuOpen(false)}>
@@ -12,19 +31,15 @@ function Menu({ menuOpen, setMenuOpen }) {
                         Quiz
                     </Link>
                 </li>
-                <li onClick={()=>setMenuOpen(false)}>
-                    <Link to='/account'>
-                        My Account
-                    </Link>
-                </li>
+                {showAccount()}
                 <li onClick={()=>setMenuOpen(false)}>
                     <Link to='/about'>
                         About Us
                     </Link>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
-                    <Link to='/about'>
-                        About Us
+                    <Link to='/blog'>
+                        Beer Blog
                     </Link>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
