81a4a49533e1ab75076af5e3d9054d23e18bb97a
JacksonWhite4725 <jawh4725@colorado.edu>
2021-12-02 19:02:16 -0700

    Completed signup form styling

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -31,55 +31,57 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
-
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+    <div className='signup'>
+      <div className='singup-container'>
+        <h2 className='signup-title'>Signup</h2>
+        <form onSubmit={handleFormSubmit} className='signup-form'>
+          <div className='signup-form-items'>
+            <label htmlFor="firstName">First Name: </label>
+            <input
+              placeholder="First"
+              name="firstName"
+              type="firstName"
+              id="firstName"
+              onChange={handleChange}
+            />
+          </div>
+          <div className='signup-form-items'>
+            <label htmlFor="lastName">Last Name: </label>
+            <input
+              placeholder="Last"
+              name="lastName"
+              type="lastName"
+              id="lastName"
+              onChange={handleChange}
+            />
+          </div>
+          <div className='signup-form-items'>
+            <label htmlFor="email">Email: </label>
+            <input
+              placeholder="youremail@test.com"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div className='signup-form-items'>
+            <label htmlFor="pwd">Password: </label>
+            <input
+              placeholder="******"
+              name="password"
+              type="password"
+              id="pwd"
+              onChange={handleChange}
+            />
+          </div>
+          <div className='signup-form-items'>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+        <p>Already have an account?</p>
+        <Link to='/login' className='login-link'>← Go to Login</Link>
+      </div>
     </div>
   );
 }
