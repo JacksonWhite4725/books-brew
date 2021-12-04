8a58795c46f7d063d8d1c68050f4a20b4c33420e
JacksonWhite4725 <jawh4725@colorado.edu>
2021-12-04 10:11:52 -0700

    Added some sample questions, need to figure out scoring system to accurately predict beer style

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -4,12 +4,30 @@ import { Link } from 'react-router-dom';
 const Quiz = () => {
     const questions = [
         {
-            questionText: 'Which of these beverages sounds most appealing to you?',
+            questionText: 'Which of these beverages sounds most appealing?',
             answerOptions: [
                 { answerText: 'Coffee' },
                 { answerText: 'Orange Juice' },
                 { answerText: 'Ginger Beer' },
                 { answerText: 'Water' }
+            ],
+        },
+        {
+            questionText: 'Which of the these foods sound most appealing?',
+            answerOptions: [
+                { answerText: 'Smoked Brisket' },
+                { answerText: 'Hand-Tossed Salad' },
+                { answerText: 'Grilled Salmon' },
+                { answerText: 'PB&J Sandwich' }
+            ],
+        },
+        {
+            questionText: 'Which of the following fruits sound most appealing?',
+            answerOptions: [
+                { answerText: 'Pumpkins' }, // Seasonal beer
+                { answerText: 'Cherries' }, // Sour pairing
+                { answerText: 'Oranges' }, // Mid SRM pairing
+                { answerText: 'Berries' } // Stout pairing
             ],
         },
     ];
@@ -62,29 +80,3 @@ const Quiz = () => {
 };
 
 export default Quiz;
-
-
-
-/*
-PSUEDOCODE QUIZ:
-
-First, we need variables to track when user's enter data in the quiz:
-- SRM (Light to dark) [1-40]: Numerical
-- IBU (Bitterness) [1-120]: Numerical
-- ABV (Alcohol content) [1-13]: Numerical
-- Sour: Boolean
-
-Next, we need a series of questions to determine user's interest in certain beers based on taste. I think it's important to categorize user's that are more knowledgable about beer and those that are relatively new.
-
-Question 1: How much beer experience would you say you've had? [Rephrase later]
-* None at all
-* A little bit
-* Quite a bit
-* A lot
-
-IF user answers "None" or "Little", send them down the beginner pipeline. Make results a little more broad to provide a box that's more diverse for exploration.
-
-IF user answers "Quite a bit" or "Lots", send them down the expert pipeline. Questions are more specific to various beers.
-
-Question 2 - Beginner Path: 
-*/
