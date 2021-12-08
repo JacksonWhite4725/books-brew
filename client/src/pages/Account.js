83342a073c4495e09a0a5f6cdeeefb410749d81d
JacksonWhite4725 <jawh4725@colorado.edu>
2021-12-07 21:19:37 -0700

    Successfully added mapping

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -10,21 +10,16 @@ const Account = () => {
         user = data.user;
     }
 
-    console.log(user);
-
     return (
         <div className='account'>
             {user ? (
                 <div className='account-container'>
                 <h1>Welcome {user.firstName} {user.lastName}!</h1>
                 <h2>Your Saved Beer Styles Are:</h2>
-                <h3>{user.styles[0].name}</h3>
-                {user.styles.map(({ _id, name, description }) => {
-                    <div className='style-card' key={_id}>
-                        <h3 className='style-name'>{name}</h3>
-                        <p className='style-description'>{description}</p>
-                    </div>
-                })}
+                <div>
+                    {user.styles.map(({ name }) => 
+                    <h3>{name}</h3>)}
+                </div>
             </div>
             ): null}
         </div>
